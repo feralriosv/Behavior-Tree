4183c4dfcb300d9afa074954ddfb74eddd6dad0c
Fernando Rios <feralriosvas1011@gmail.com>
2025-09-08 00:38:53 +0200

    s

--- src/model/decisiontree/node/CompositeNode.java
+++ src/model/decisiontree/node/CompositeNode.java
@@ -46,7 +46,7 @@ public class CompositeNode extends Node<CompositeType> {
         Node<?> prevActive = getTree().getLastTicked();
         super.tick(context);
 
-        if (prevActive != this && this.getLastState() != TickState.ENTRY) {
+        if (prevActive != this && this.getLastState() != TickState.ENTRY && this.getLastState() != TickState.STAND_BY) {
             saveState(context, TickState.ENTRY);
             this.setLastState(TickState.ENTRY);
         }
